d2477e0890d42f830f65a24844bac7331c1341a3
Karl Wright <kwright@apache.org>
2013-03-26 06:54:49 +0000

    Aha, the problem is that the default values are not working.
    git-svn-id: https://svn.apache.org/repos/asf/manifoldcf/integration/elasticsearch/trunk@1460991 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/manifoldcf/elasticsearch/MCFAuthorizerTest.java
+++ src/test/java/org/apache/manifoldcf/elasticsearch/MCFAuthorizerTest.java
@@ -89,6 +89,14 @@ public class MCFAuthorizerTest
   @Test
   public void simpleTest() throws Exception
   {
+    // Sanity check to be sure I indexed everything right...
+    SearchResponse allResponse = client.prepareSearch("test").setQuery(QueryBuilders.matchAllQuery()).execute().actionGet();
+    verifyResponse(allResponse, "da12", "da13-dd3", "sa123-sd13", "sa3-sd1-da23", "notoken");
+    SearchResponse partialResponse = client.prepareSearch("test").setQuery(QueryBuilders.termQuery("allow_token_share","token1")).execute().actionGet();
+    verifyResponse(partialResponse, "sa123-sd13");
+    SearchResponse nullResponse = client.prepareSearch("test").setQuery(QueryBuilders.termQuery("allow_token_share","__nosecurity__")).execute().actionGet();
+    verifyResponse(nullResponse, "da12", "da13-dd3", "notoken");
+
     ConfigurationParameters cp = new ConfigurationParameters();
     MCFAuthorizer mcfa = new MCFAuthorizer(cp);
     FilterBuilder user1Filter = mcfa.buildAuthorizationFilter("user1");
@@ -96,11 +104,6 @@ public class MCFAuthorizerTest
     FilterBuilder user3Filter = mcfa.buildAuthorizationFilter("user3");
     FilterBuilder user4Filter = mcfa.buildAuthorizationFilter("user4");
     
-    // Sanity check to be sure I indexed everything right...
-    SearchResponse allResponse = client.prepareSearch("test").setQuery(QueryBuilders.matchAllQuery()).execute().actionGet();
-    verifyResponse(allResponse, "da12", "da13-dd3", "sa123-sd13", "sa3-sd1-da23", "notoken");
-    SearchResponse partialResponse = client.prepareSearch("test").setQuery(QueryBuilders.termQuery("allow_token_share","token1")).execute().actionGet();
-    verifyResponse(partialResponse, "sa123-sd13");
     // Ok, check the filters I built.
     SearchResponse user1Response = client.prepareSearch("test").setQuery(QueryBuilders.matchAllQuery()).setFilter(user1Filter).execute().actionGet();
     verifyResponse(user1Response, "da12", "da13-dd3", "notoken");
